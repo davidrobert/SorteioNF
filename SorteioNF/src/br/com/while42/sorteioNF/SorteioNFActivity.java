105110ebd6d71b784a01fe897793ab8f911316df
David Robert <davidrobert@gmail.com>
2011-11-17 23:00:55 -0200

    Correcao do tratamento de erros Melhoria no layout

--- SorteioNF/src/br/com/while42/sorteioNF/SorteioNFActivity.java
+++ SorteioNF/src/br/com/while42/sorteioNF/SorteioNFActivity.java
@@ -16,16 +16,22 @@ public class SorteioNFActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		Spinner s = (Spinner) findViewById(R.id.sizeSpinner);
-	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-	            this, R.array.sizes, android.R.layout.simple_spinner_item);
-	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);	    
-	    s.setAdapter(adapter);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.sizes, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);	    
+		s.setAdapter(adapter);
 
 		final Button button = (Button) findViewById(R.id.calcButton);
 		final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
 		final AlertDialog errorDialog = new AlertDialog.Builder(this).create();
+
+		errorDialog.setMessage("Dados invalidos");
+		errorDialog.setButton("Voltar", new DialogInterface.OnClickListener() {  
+			public void onClick(DialogInterface dialog, int which) {  
+				return;  
+			} });
 
 		button.setOnClickListener(new View.OnClickListener() {
 
@@ -33,33 +39,31 @@ public class SorteioNFActivity extends Activity {
 
 				final EditText costText = (EditText) findViewById(R.id.costText);
 				final EditText sizeText = (EditText) findViewById(R.id.sizeText); 
-				try{
-					double cost = new Double(costText.getText().toString());
 
-					long numberOfPeople = (new Integer(sizeText.getText().toString())).intValue();
-
-					Integer result = (int)NFPCalc.calculateForToday(cost, numberOfPeople);
-
-					alertDialog.setTitle("Resultado do Sorteio");  
-					alertDialog.setMessage(result.toString());  
-					alertDialog.setButton("OK", new DialogInterface.OnClickListener() {  
-						public void onClick(DialogInterface dialog, int which) {  
-							return;  
-						} });
-					
-					alertDialog.show();
-					
-				} catch (Exception e) {
-					errorDialog.setMessage("Dados invalidos");
-					errorDialog.setButton("Voltar", new DialogInterface.OnClickListener() {  
-						public void onClick(DialogInterface dialog, int which) {  
-							return;  
-						} });
-					
+				if (costText.getText().toString().length() == 0 || sizeText.getText().toString().length() == 0) {
 					errorDialog.show();
+					return;
 				}
 
-				
+				double cost = new Double(costText.getText().toString());
+
+				long numberOfPeople = (new Integer(sizeText.getText().toString())).intValue();
+
+				if (numberOfPeople == 0) {
+					errorDialog.show();
+					return;
+				}
+
+				Integer result = (int)NFPCalc.calculateForToday(cost, numberOfPeople);
+
+				alertDialog.setTitle("Resultado do Sorteio");  
+				alertDialog.setMessage(result.toString());  
+				alertDialog.setButton("OK", new DialogInterface.OnClickListener() {  
+					public void onClick(DialogInterface dialog, int which) {  
+						return;  
+					} });
+
+				alertDialog.show();
 			}
 		});
 	}
